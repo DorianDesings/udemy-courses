a9f00a14c6b8c42e4e27fab71423bf0b329402b6
sheilitamr <sheilitamr@gmail.com>
2020-05-01 12:35:52 +0200

    Añadido mensaje error

--- public/js/dbManager.js
+++ public/js/dbManager.js
@@ -1,5 +1,5 @@
 const form = document.getElementById('form')
-//const error = document.getElementById('error')
+const error = document.getElementById('error')
 const db = firebase.firestore();
 
 const addUsers = (user, email) => {
@@ -9,21 +9,23 @@ const addUsers = (user, email) => {
     })
         .then(function (docRef) {
             console.log("Document written with ID: ", docRef.id);
-            // clearErrors()
+            clearErrors()
             form.reset()
-            modal.classList.remove('modal--show')
+            setTimeout(() => {
+                modal.classList.remove('modal--show')
+            }, 1000)
         })
         .catch(function (error) {
             console.error("Error adding document: ", error);
         });
 }
 
-// const showErrors = (msg) => {
-//     error.textContent = msg
-// }
-// const clearErrors = () => {
-//     error.textContent = ''
-// }
+const showErrors = (msg) => {
+    error.textContent = msg
+}
+const clearErrors = () => {
+    error.textContent = ''
+}
 
 
 form.addEventListener('submit', (e) => {
@@ -38,16 +40,18 @@ form.addEventListener('submit', (e) => {
         db.collection("users").onSnapshot((querySnapshot) => {
             querySnapshot.forEach((doc) => {
                 if (doc.data().email == email) {
+                    showErrors('El correo ya existe')
                     errors++
                     return
                 }
             })
             if (errors == 0) {
                 addUsers(user, email)
-
             }
         });
 
+    } else {
+        showErrors('Rellene los campos correctamente')
     }
 
 })
